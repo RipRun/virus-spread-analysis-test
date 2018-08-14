4bc64676dc0ddf6727b6397332a8eca76047a30f
RipRun <ripple.erin@comcast.net>
2018-08-14 03:17:50 -0700

    added new files

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ size = (width, height) = (850, 480)
 screen = pygame.display.set_mode(size)
 clock = pygame.time.Clock()
 color = (26, 255, 255)
+font = pygame.font.SysFont(None, 70)
+split_times = 10
+
 
 bacteria = pygame.sprite.Group()
 doctors = pygame.sprite.Group()
@@ -16,6 +19,12 @@ doc_num = 1
 bac_num = 5
 split_time = 500
 done = False
+
+def init():
+    for i in range(bac_num):
+        bacteria.add(VirusNode((random.randint(50, width - 50), random.randint(50, height - 50)), split_time))
+    for i in range(doc_num):
+        doctors.add(Doctor((random.randint(50, width - 50), random.randint(50, height - 50))))
 
 
 def process_events():
@@ -26,14 +35,25 @@ def process_events():
 
 
 def main():
-    bacteria.add(VirusNode((random.randint(50, width-50), random.randint(50, height-50)), split_time))
-    doctors.add(Doctor((random.randint(50, width-50), random.randint(50, height-50))))
+    init()
     while not done:
         clock.tick(60)
         process_events()
+        if len(bacteria) > bac_num * split_times:
+            text = font.render("You were overrun", True, (255, 0, 0))
+            text_rect = text.get_rect()
+        elif len(bacteria) == 0:
+            text = font.render("Outbreak stopped", True, (255, 0, 0))
+        else:
+            doctors.update()
+            bacteria.update()
+            pygame.sprite.groupcollide(doctors, bacteria, False, True)
+            text = font.render("Bacteria Count: {}".format(len(bacteria)), True, (255, 0, 0))
+            text_rect = text.get_rect()
         screen.fill(color)
         doctors.draw(screen)
         bacteria.draw(screen)
+        screen.blit(text, text_rect)
         pygame.display.flip()
 
 
